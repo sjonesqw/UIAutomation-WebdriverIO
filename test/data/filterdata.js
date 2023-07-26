2ba2477dc17afe2f6268c7ffe1d28454c860655d
sjonesqw <sjones@qualityworkscg.com>
2023-07-25 23:06:23 -0400

    Completed filter and sort tests and updated package.json

---
+++ test/data/filterdata.js
@@ -0,0 +1,32 @@
+module.exports =
+    
+    [
+        
+        {
+            filterTitle : "lowToHigh",
+            top: "$15",
+            bottom: "$2000"
+            
+        },
+        {
+            filterTitle : "highToLow",
+            top: "$2000",
+            bottom: "$15"
+            
+        },
+        {
+            filterTitle : "zToA",
+            top: "White Couch",
+            bottom: "Dell Laptop"
+            
+            
+        },
+        {
+            filterTitle : "aToZ",
+            top: "Dell Laptop",
+            bottom: "White Couch"
+            
+            
+        }
+
+    ];
