35ce5d2b6429c512d1132b687a523c67cd7b0ab1
sjonesqw <76572707+sjonesqw@users.noreply.github.com>
2023-07-28 03:51:39 -0400

    Updated camelcase

--- test/specs/search.e2e.js
+++ test/specs/search.e2e.js
@@ -1,18 +1,18 @@
-const LoginPage = require('../pageobjects/Authentication/login.page');
+const loginPage = require('../pageobjects/Authentication/login.page');
 const search = require('../pageobjects/search.page');
-const searchdata = require('../data/searchData');
+const searchData = require('../data/searchData');
 
 
 describe('My filter & sort test ', () => {
     it ('should login to the application', async () =>{
 
-        await LoginPage.open();
-        await LoginPage.login("test@mailinator.com", "Stephanie1!");
+        await loginPage.open();
+        await loginPage.login("test@mailinator.com", "Stephanie1!");
         await expect( browser).toHaveUrl('https://ui-automation-camp.vercel.app/products');
 
     });
     
-    for (const inputterm of searchdata){
+    for (const inputterm of searchData){
         it(' should naviagate to the product gallery page and search for items from the search data file', async () => {
             
             //loop thru search terms and verify that results displayed match the search term entered
