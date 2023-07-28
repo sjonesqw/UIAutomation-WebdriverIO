8cf1a42198e8cae2f04c1a122bdbb79a6847daa9
sjonesqw <sjones@qualityworkscg.com>
2023-07-28 01:56:54 -0400

    code cleanup and added comments

--- test/specs/cart.e2e.js
+++ test/specs/cart.e2e.js
@@ -1,6 +1,6 @@
-const LoginPage = require('../pageobjects/Authentication/login.page');
-const addtocart = require('../pageobjects/addtocart.page');
-const cartview = require('../pageobjects/cart.page');
+const loginPage = require('../pageobjects/Authentication/login.page');
+const addToCart = require('../pageobjects/addtocart.page');
+const cartView = require('../pageobjects/cart.page');
 
 
 
@@ -8,18 +8,23 @@ const cartview = require('../pageobjects/cart.page');
 describe('My cart view test ', () => {
 
     it('should login to the application, add an item to the cart, navigate to the cart increase and decrease the quantity, then delete item from cart', async () => {
-        await LoginPage.open();
-        await LoginPage.login("test@mailinator.com", "Stephanie1!");
+        await loginPage.open();
+        await loginPage.login("test@mailinator.com", "Stephanie1!");
         await browser.pause(2000);
         await expect(browser).toHaveUrl('https://ui-automation-camp.vercel.app/products');
-        await addtocart.addSingleItem1();
-        await cartview.changeQuantity();
-        await expect(cartview.productTitle).toHaveTextContaining("Quality Fitted Hat");
-        await expect (cartview.productDescription).toHaveTextContaining("3 Pack Unisex Vintage Washed Distressed Baseball-Cap");
+        //add a single item to the cart
+        await addToCart.addSingleItem1();
+        //navigate to the detail cart viewn and chage the quantity of the item using the plus/minus buttons
+        await cartView.changeQuantity();
+        //verify that the product title and description are displayed
+        await expect(cartView.productTitle).toHaveTextContaining("Quality Fitted Hat");
+        await expect (cartView.productDescription).toHaveTextContaining("3 Pack Unisex Vintage Washed Distressed Baseball-Cap");
         await expect(browser).toHaveUrl('https://ui-automation-camp.vercel.app/products#/cart');
-        await cartview.viewAndDeleteFromDetail();
+        //delete the item from the cart
+        await cartView.viewAndDeleteFromDetail();
         await browser.pause(2000);
-        await expect(cartview.emptyCart).toHaveTextContaining("Your cart is empty");
+        //verify that text id displayed indicating that the cart is empty
+        await expect(cartView.emptyCart).toHaveTextContaining("Your cart is empty");
 
         
         
